3a3396b04d35ca7b2c9eaeff2e52250f0f3f0bf1
Mr. K4US4R <ahkausar2@gmail.com>
2022-07-10 19:52:36 +0600

    Add files via upload

---
+++ Fuck_ISLAMTIAIB_4.py
@@ -0,0 +1,83 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+class Main:
+	def __init__(self):
+		self.id = []
+		self.ok = []
+		self.cp = []
+		self.loop = 0
+		os.system("clear")
+		print(logo)
+		print("")
+		print("\033[1;97m═══════════════════════════════════════════════════════════")
+		print("\033[1;37m𝗡𝗢𝗧𝗘 : First Follow My GitHub Please 🙏")
+		print("\033[1;97m═══════════════════════════════════════════════════════════")
+		
+		
+		print("")
+		print("\033[1;37m [1] 𝗙𝗶𝗿𝘀𝘁 Please Follow My GitHub ")
+		print("\033[1;37m [2] 𝗘𝘅𝗶𝘁")
+		print("")
+		Baloch = input("\n\033[1;37m  Choose : \033[1;32m")
+		if Baloch in ["", " "]:
+			exit()
+		elif Baloch in ["2", "02"]:
+			print("    Thanks🐉")
+			exit()
+		elif Baloch in ["1", "01"]:
+			os.system("xdg-open https://github.com/MrK4US4R ")
+			print("")
+			time.sleep(3.0)
+			print("\033[1;37m    𝗖𝗛𝗘𝗖𝗞𝗜𝗡𝗚 𝗔𝗣𝗣𝗥𝗢𝗩𝗔𝗟 ")
+			print("")
+			input("\n\033[1;37m TYPE Your Ex Name OK ➣\033[1;37m")
+			time.sleep(3.1)
+			print("")
+			print("\033[1;32m WELCOME TO •••KAUSAR••• FREE TOOL")
+			time.sleep(3.0)
+			os.system("clear")
+
+
+if __name__ == '__main__':
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
+    cok.Brute().moch_yayan()
+    Main()
+
+try:Main()
+except Exception as e:exit(str(e))
+
+import requests,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    print('\n\n\n\n\n FUCK 👉ISLAMTIAIB👈 IN PROGRESS...!')
+    time.sleep(2)
+    os.system('clear')
+    print('\n\n\n\n\n BYPASS BY 💁 MrKAUSAR Respect Bro...!')
+    time.sleep(6)
+
+import os
+try:
+    import requests
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
+    os.system('pip install requests')
+
+try:
+    import bs4
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n')
+    os.system('pip install bs4')
+
+try:
+    import rich
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
+    os.system('pip install rich')
+#################################################################################
+import os
+from src import cok
